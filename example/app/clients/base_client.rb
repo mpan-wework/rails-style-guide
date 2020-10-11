b0a678809419b493813aad3d77bd4c0f1161a2e8
mpan-wework <mike.pan@wework.com>
2020-10-12 07:28:36 +0800

    feat: add RegionClient to fetch region info

--- example/app/clients/base_client.rb
+++ example/app/clients/base_client.rb
@@ -1,4 +1,6 @@
 class BaseClient
+  class GraphqlQueryError < ::StandardError; end
+
   def initialize(baseurl:, token: '')
     @baseurl = baseurl
     @token = token
@@ -16,5 +18,22 @@ class BaseClient
 
   def bearer_authorization
     "Bearer #{@token}"
+  end
+
+  def graphql_query(query:, variables: {}, url: 'graphql', headers: {})
+    headers = { 'Content-Type': 'application/json' }.merge headers
+    response = conn.post do |req|
+      req.url url
+      req.body = { query: query, variables: variables }.to_json
+      headers.each { |header, content| req.headers[header] = content }
+    end
+
+    raise GraphqlQueryError, response.body unless response.success?
+
+    parse_response response
+  end
+
+  def parse_response(response)
+    JSON.parse(response.body).deep_transform_keys(&:underscore).deep_symbolize_keys
   end
 end
